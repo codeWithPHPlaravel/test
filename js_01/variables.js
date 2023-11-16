8d563edd8cfe8610989afacd040c75821722e640
Laravel and Chai <147817602+codeWithPHPlaravel@users.noreply.github.com>
2023-11-16 09:35:10 +0000

    variables

---
+++ js_01/variables.js
@@ -0,0 +1,13 @@
+const accountId= 12345
+let accountEmail= "ghr@gmail.com"
+var accountName= "Ghulam Rasool"
+accountCity= "Khi"
+// accountId= 4 // Not working
+//console.log(accountId);
+
+ accountEmail='updated@gmail.com'
+var accountName='Updated Name'
+accountCity='Updated City'
+
+console.table([accountId,accountEmail,accountName,accountCity])
+
